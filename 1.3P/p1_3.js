2444a28ab1274cf4591f03a58249aaaa0cead513
MeetTejani0 <151134775+MeetTejani0@users.noreply.github.com>
2025-11-21 18:37:03 +1100

    add html + change js file

--- 1.3P/p1_3.js
+++ 1.3P/p1_3.js
@@ -1,8 +1,11 @@
-let number = Math.floor(Math.random() * 10) + 1;
-let guess = prompt("Guess a number between 1 and 10:");
+function changeText(){
+    var textsArray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"];
+    var number = getRandomNumberBetween(0, textsArray.length - 1);
 
-if (parseInt(guess) === number) {
-  console.log("You Won!!!");
-} else {
-  console.log("You Lost, Answer is " + number);
+    console.log("Index: ", number);
+    document.getElementById("heading").innerHTML = textsArray[number];
+}
+
+function getRandomNumberBetween(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
